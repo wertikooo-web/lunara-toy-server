f7bdabe8b28155c4c61d0a3de7170f31cb56bd14
wertikooo-web <wertikooo@gmail.com>
2026-07-08 18:23:34 +0300

    Treat English no as ambiguous in STT smoke test

--- scripts/smoke-stt-short-phrases.js
+++ scripts/smoke-stt-short-phrases.js
@@ -6,7 +6,7 @@ const stt = require('../modules/stt');
 assert.strictEqual(stt.sanitizeShortRussianTranscript('Yes.', { language: 'ru-RU' }), 'да');
 assert.strictEqual(stt.sanitizeShortRussianTranscript('yeah', { language: 'ru-RU' }), 'да');
 assert.strictEqual(stt.sanitizeShortRussianTranscript('OK', { language: 'ru-RU' }), 'окей');
-assert.strictEqual(stt.sanitizeShortRussianTranscript('No.', { language: 'ru-RU' }), 'нет');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('No.', { language: 'ru-RU' }), 'no');
 assert.strictEqual(stt.sanitizeShortRussianTranscript('you', { language: 'ru-RU' }), '');
 assert.strictEqual(stt.sanitizeShortRussianTranscript('Thank you.', { language: 'ru-RU' }), '');
 assert.strictEqual(stt.sanitizeShortRussianTranscript('да', { language: 'ru-RU' }), 'да');
