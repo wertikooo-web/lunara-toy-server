8bf0b571eaf4f84473ba226ad9993da6e1b1bc8f
wertikooo-web <wertikooo@gmail.com>
2026-07-08 18:22:38 +0300

    Add STT short phrase smoke test

---
+++ scripts/smoke-stt-short-phrases.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+const stt = require('../modules/stt');
+
+assert.strictEqual(stt.sanitizeShortRussianTranscript('Yes.', { language: 'ru-RU' }), 'да');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('yeah', { language: 'ru-RU' }), 'да');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('OK', { language: 'ru-RU' }), 'окей');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('No.', { language: 'ru-RU' }), 'нет');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('you', { language: 'ru-RU' }), '');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('Thank you.', { language: 'ru-RU' }), '');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('да', { language: 'ru-RU' }), 'да');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('окей', { language: 'ru-RU' }), 'да');
+assert.strictEqual(stt.sanitizeShortRussianTranscript('No.', { language: 'en-US' }), 'No');
+
+console.log('[Smoke] stt short phrase sanitizer ok');
